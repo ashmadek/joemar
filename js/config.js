f11a225e51d6dec88c4de4e0bc596eeda201db36
ashmadek <lyzerg_diethel@outlook.com>
2020-01-10 17:09:47 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_FikE792nU', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ppu0i68u66236n1h6dfa8l00t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5bcinl8as9pelfqrrdl9ia3eb8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
